d6902b0696fb3ec88c782070ac02a7c90278ef98
鄭允頎 <kiyume@ntu.im>
2021-05-12 21:14:04 +0800

    Fix errors, refactor jwt to standalone pyton file

--- middleware/auth.py
+++ middleware/auth.py
@@ -1,41 +1,18 @@
 from dataclasses import dataclass
-from datetime import datetime, timedelta
-from functools import wraps
-from itertools import chain
 
 import fastapi
-import jwt
 
 from base.cls import DataclassBase
 from base.enum import RoleType
 import exceptions as exc
 from persistence import database as db
-
-
-SECRET = 'aaa'  # TODO
-DEFAULT_VALID = timedelta(days=7)  # TODO
+from util import jwt
 
 
 @dataclass
 class Account(DataclassBase):
     id: int
     role: RoleType
-
-
-async def gen_jwt(account_id: int, expire: timedelta = DEFAULT_VALID) -> str:
-    return jwt.encode({
-        'account-id': account_id,
-        'expire': (datetime.now() + expire).isoformat(),
-    }, key=SECRET)
-
-
-async def validate_jwt(encoded: str) -> Account:
-    decoded = jwt.decode(encoded, key=SECRET)
-    expire = datetime.fromisoformat(decoded['expire'])
-    if expire > datetime.now():
-        raise exc.LoginExpired
-    account_id = decoded['account-id']
-    return Account(id=account_id, role=await db.rbac.get_system_role_by_account_id(account_id))
 
 
 class Middleware:
@@ -47,19 +24,15 @@ class Middleware:
 
     async def __call__(self, scope, receive, send) -> None:
         if scope["type"] not in ["http", "websocket"]:
+            await self.app(scope, receive, send)
             return
 
-        try:
-            assert scope["type"] in ["http", "websocket"]
-        except AssertionError:
-            return
-        else:
-            request = fastapi.Request(scope)
-            auth_token = request.headers.get('auth-token', None)
-            if authed_account := await validate_jwt(auth_token):
-                scope['authed_account'] = authed_account
-        finally:
-            await self.app(scope, receive, send)
+        request = fastapi.Request(scope)
+        auth_token = request.headers.get('auth-token', None)
+        if account_id := await jwt.decode(auth_token):
+            scope['authed_account'] = Account(id=account_id,
+                                              role=await db.rbac.get_system_role_by_account_id(account_id))
+        await self.app(scope, receive, send)
 
 
 class Request(fastapi.Request):
