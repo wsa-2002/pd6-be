5d706b3c27f8cf851f63e7358b07a34e6dac6e83
鄭允頎 <kiyume@ntu.im>
2021-06-20 17:54:26 +0800

    Splite testcase api -> testcase + input data + output data

--- api/problem.py
+++ api/problem.py
@@ -1,3 +1,4 @@
+from dataclasses import dataclass
 from typing import Optional, Sequence
 
 from pydantic import BaseModel
@@ -67,8 +68,6 @@ async def delete_problem(problem_id: int):
 class AddTestcaseInput(BaseModel):
     is_sample: bool
     score: int
-    input_file: str  # TODO
-    output_file: str  # TODO
     time_limit: int
     memory_limit: int
     is_disabled: bool
@@ -77,11 +76,33 @@ class AddTestcaseInput(BaseModel):
 @router.post('/problem/{problem_id}/testcase', tags=['Testcase'])
 async def add_testcase_under_problem(problem_id: int, data: AddTestcaseInput) -> int:
     return await db.testcase.add(problem_id=problem_id, is_sample=data.is_sample, score=data.score,
-                                 input_file=data.input_file, output_file=data.output_file,
+                                 input_file=None, output_file=None,
                                  time_limit=data.time_limit, memory_limit=data.memory_limit,
                                  is_disabled=data.is_disabled)
 
 
+@dataclass
+class ReadTestcaseOutput:
+    id: int
+    problem_id: int
+    is_sample: bool
+    score: int
+    time_limit: int
+    memory_limit: int
+    is_disabled: bool
+    is_deleted: bool
+
+
 @router.get('/problem/{problem_id}/testcase', tags=['Testcase'])
-async def browse_testcase_under_problem(problem_id: int) -> Sequence[do.Testcase]:
-    return await db.testcase.browse(problem_id=problem_id)
+async def browse_testcase_under_problem(problem_id: int) -> Sequence[ReadTestcaseOutput]:
+    testcases = await db.testcase.browse(problem_id=problem_id)
+    return [ReadTestcaseOutput(
+        id=testcase.id,
+        problem_id=testcase.problem_id,
+        is_sample=testcase.is_sample,
+        score=testcase.score,
+        time_limit=testcase.time_limit,
+        memory_limit=testcase.memory_limit,
+        is_disabled=testcase.is_disabled,
+        is_deleted=testcase.is_deleted,
+    ) for testcase in testcases]
--- persistence/database/testcase.py
+++ persistence/database/testcase.py
@@ -1,4 +1,4 @@
-from typing import Sequence
+from typing import Sequence, Optional
 
 import log
 from base import do
@@ -6,7 +6,7 @@ from base import do
 from .base import SafeExecutor
 
 
-async def add(problem_id: int, is_sample: bool, score: int, input_file: str, output_file: str,
+async def add(problem_id: int, is_sample: bool, score: int, input_file: Optional[str], output_file: Optional[str],
               time_limit: int, memory_limit: int, is_disabled: bool) -> int:
     async with SafeExecutor(
             event='Add testcase',
@@ -26,18 +26,16 @@ async def add(problem_id: int, is_sample: bool, score: int, input_file: str, out
 async def read(testcase_id: int, include_disabled=False, include_deleted=False) -> do.Testcase:
     async with SafeExecutor(
             event='read testcases with problem id',
-            sql=fr'SELECT problem_id, is_sample, score, input_file, output_file, '
-                fr'       time_limit, memory_limit, is_disabled, is_deleted'
+            sql=fr'SELECT problem_id, is_sample, score, time_limit, memory_limit, is_disabled, is_deleted'
                 fr'  FROM testcase'
                 fr' WHERE id = %(testcase_id)s'
                 fr'{" AND NOT is_disabled" if not include_disabled else ""}'
                 fr'{" AND NOT is_deleted" if not include_deleted else ""}',
             testcase_id=testcase_id,
             fetch=1,
-    ) as (problem_id, is_sample, score, input_file, output_file,
-          time_limit, memory_limit, is_disabled, is_deleted):
+    ) as (problem_id, is_sample, score, time_limit, memory_limit, is_disabled, is_deleted):
         return do.Testcase(id=testcase_id, problem_id=problem_id, is_sample=is_sample, score=score,
-                           input_file=input_file, output_file=output_file,
+                           input_file=None, output_file=None,
                            time_limit=time_limit, memory_limit=memory_limit,
                            is_disabled=is_disabled, is_deleted=is_deleted)
 
@@ -45,8 +43,7 @@ async def read(testcase_id: int, include_disabled=False, include_deleted=False) 
 async def browse(problem_id: int, include_disabled=False, include_deleted=False) -> Sequence[do.Testcase]:
     async with SafeExecutor(
             event='browse testcases with problem id',
-            sql=fr'SELECT id, is_sample, score, input_file, output_file, '
-                fr'       time_limit, memory_limit, is_disabled, is_deleted'
+            sql=fr'SELECT id, is_sample, score, time_limit, memory_limit, is_disabled, is_deleted'
                 fr'  FROM testcase'
                 fr' WHERE problem_id = %(problem_id)s'
                 fr'{" AND NOT is_disabled" if not include_disabled else ""}'
@@ -56,11 +53,10 @@ async def browse(problem_id: int, include_disabled=False, include_deleted=False)
             fetch='all',
     ) as records:
         return [do.Testcase(id=id_, problem_id=problem_id, is_sample=is_sample, score=score,
-                            input_file=input_file, output_file=output_file,
+                            input_file=None, output_file=None,
                             time_limit=time_limit, memory_limit=memory_limit,
                             is_disabled=is_disabled, is_deleted=is_deleted)
-                for (id_, problem_id, is_sample, score, input_file, output_file,
-                     time_limit, memory_limit, is_disabled, is_deleted)
+                for (id_, problem_id, is_sample, score, time_limit, memory_limit, is_disabled, is_deleted)
                 in records]
 
 
