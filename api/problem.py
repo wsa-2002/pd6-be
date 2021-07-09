8033f06f81e1cd0273ce91a9247d0ff135a828aa
何品諭 <polly@ntu.im>
2021-07-09 14:17:14 +0800

    get publicize time first

--- api/problem.py
+++ api/problem.py
@@ -46,12 +46,12 @@ async def read_problem(problem_id: int, request: auth.Request) -> do.Problem:
     is_class_manager = await rbac.validate(request.account.id, RoleType.manager, class_id=challenge.class_id)
 
     is_challenge_publicized = False
-    if challenge.publicize_type == ChallengePublicizeType.start_time:
-        if datetime.now() >= challenge.start_time:
-            is_challenge_publicized = True
+    publicize_time = challenge.start_time
     if challenge.publicize_type == ChallengePublicizeType.end_time:
-        if datetime.now() >= challenge.end_time:
-            is_challenge_publicized = True
+        publicize_time = challenge.end_time
+
+    if datetime.now() >= publicize_time:
+        is_challenge_publicized = True
 
     if not ((problem.is_hidden and is_class_manager)
             or (not problem.is_hidden and is_system_normal and is_challenge_publicized)):
