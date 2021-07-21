b95544988e02ec421fe8c2bc98fe48e2ef5e10c2
Yang Cheng <an4976887@ntu.im>
2021-07-21 18:41:36 +0800

    send multiple email at once

--- persistence/email/notification.py
+++ persistence/email/notification.py
@@ -6,10 +6,13 @@ from typing import Sequence
 
 
 # for general msgs
-async def send(to: str, msg: str, subject='PDOGS Notification'):
+async def send(to: str = None, msg: str = "", bcc: str = None, subject='PDOGS Notification'):
     message = EmailMessage()
     message["From"] = f"{smtp_config.username}@{smtp_config.host}"
-    message["To"] = to
+    if to:
+        message["To"] = to
+    if bcc:
+        message["Bcc"] = bcc
     message["Subject"] = subject
     message.set_content(msg)
 
@@ -19,12 +22,12 @@ async def send(to: str, msg: str, subject='PDOGS Notification'):
 
 # update class manager change
 async def notify_cm_change(tos: Sequence[str], account_id: int, class_id: int, operator_id: int):
-    for to in tos:
-        msg = fr"""
+    bccs = ', '.join(tos)
+    msg = fr"""
 Class Manager Has Been Updated:
 Class ID: {class_id}
 Added CM: {account_id}
 Operator: {operator_id}        
 """
-        subject = "PDOGS Notification (Class Manager Updates)"
-        await send(to, msg, subject)
+    subject = "PDOGS Notification (Class Manager Updates)"
+    await send(bcc=bccs, msg=msg, subject=subject)
