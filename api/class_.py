be2b1186c233f95525325a03e4206678302de8d6
Yang Cheng <an4976887@ntu.im>
2021-07-27 13:30:11 +0800

    fix comments

--- api/class_.py
+++ api/class_.py
@@ -124,12 +124,12 @@ async def edit_class_member(class_id: int, data: Sequence[EditClassMemberInput],
     if not await rbac.validate(request.account.id, RoleType.manager, class_id=class_id, inherit=True):
         raise exc.NoPermission
 
-    for (member_id, role) in data:
-        await db.class_.edit_member(class_id=class_id, member_id=member_id, role=role)
+    for item in data:
+        await db.class_.edit_member(class_id=class_id, member_id=item.member_id, role=item.role)
 
-    class_manager_emails = await db.class_.browse_member_emails(class_id, RoleType.manager)
     updated_class_managers = [member for member in data if member.role is RoleType.manager]
     if updated_class_managers:
+        class_manager_emails = await db.class_.browse_member_emails(class_id, RoleType.manager)
         await email.notification.notify_cm_change(class_manager_emails, updated_class_managers,
                                                   class_id, request.account.id)
 
