9d63fe350c028b355077b265c0f363476fb9dab2
何品諭 <polly@ntu.im>
2021-07-29 20:49:41 +0800

    adjust s3 and add BREAD in db

--- persistence/database/s3_file.py
+++ persistence/database/s3_file.py
@@ -1,9 +1,20 @@
 from typing import Sequence
 
-import log
 from base import do
-
 from .base import SafeExecutor
+
+
+async def browse() -> Sequence[do.S3File]:
+    async with SafeExecutor(
+            event='browse s3_file',
+            sql=fr'SELECT id, bucket, key'
+                fr'  FROM s3_file'
+                fr' ORDER BY id ASC',
+            fetch='all',
+    ) as records:
+        return [do.S3File(id=id_, bucket=bucket, key=key)
+                for (id_, bucket, key)
+                in records]
 
 
 async def read(s3_file_id: int) -> do.S3File:
@@ -41,6 +52,32 @@ async def add(bucket: str, key: str) -> int:
             fetch=1,
     ) as (id_,):
         return id_
+
+
+async def edit(s3_file_id: int,
+               bucket: str = None,
+               key: str = None) -> None:
+    to_updates = {}
+
+    if bucket is not None:
+        to_updates['bucket'] = bucket
+    if key is not None:
+        to_updates['key'] = key
+
+    if not to_updates:
+        return
+
+    set_sql = ', '.join(fr"{field_name} = %({field_name})s" for field_name in to_updates)
+
+    async with SafeExecutor(
+            event='edit s3_file by id',
+            sql=fr'UPDATE s3_file'
+                fr'   SET {set_sql}'
+                fr' WHERE id = %(s3_file_id)s',
+            s3_file_id=s3_file_id,
+            **to_updates,
+    ):
+        pass
 
 
 async def delete(s3_file_id: int) -> None:
--- persistence/s3/testcase.py
+++ persistence/s3/testcase.py
@@ -4,7 +4,6 @@ import uuid
 
 from . import s3_handler
 
-
 _BUCKET_NAME = 'testcase'
 
 
@@ -26,22 +25,3 @@ async def upload_output(file: typing.IO, filename: str, testcase_id: int) -> Tup
     key = f'{testcase_id}/output-data/{uuid.uuid4()}/{filename}'
     await bucket.upload_fileobj(file, key)
     return _BUCKET_NAME, key
-
-
-async def update(file: typing.IO, key: str) -> Tuple[str, str]:
-    """
-    :return: bucket name and key
-    """
-    bucket = await s3_handler.get_bucket(_BUCKET_NAME)
-    await bucket.upload_fileobj(file, key)
-    return _BUCKET_NAME, key
-
-
-async def delete(bucket: str, key: str) -> None:
-    to_delete_file = await s3_handler.resource.Object(bucket, key)
-    await to_delete_file.delete()
-
-
-async def download(key: str, filename: str) -> None:
-    bucket = await s3_handler.get_bucket(_BUCKET_NAME)
-    await bucket.download_file(key, filename)
