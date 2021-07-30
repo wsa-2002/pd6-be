cfc9977c3f273a559a34634c286e641e01b7fcc3
何品諭 <polly@ntu.im>
2021-07-30 18:28:16 +0800

    fix comments

--- persistence/database/s3_file.py
+++ persistence/database/s3_file.py
@@ -29,18 +29,6 @@ async def read(s3_file_id: int) -> do.S3File:
         return do.S3File(id=id_, bucket=bucket, key=key)
 
 
-async def read_by_key(key: str) -> do.S3File:
-    async with SafeExecutor(
-            event='read s3_file by key',
-            sql=fr'SELECT id, bucket, key'
-                fr'  FROM s3_file'
-                fr' WHERE key = %(key)s',
-            key=key,
-            fetch=1,
-    ) as (id_, bucket, key):
-        return do.S3File(id=id_, bucket=bucket, key=key)
-
-
 async def add(bucket: str, key: str) -> int:
     async with SafeExecutor(
             event='add s3_file',
@@ -52,39 +40,3 @@ async def add(bucket: str, key: str) -> int:
             fetch=1,
     ) as (id_,):
         return id_
-
-
-async def edit(s3_file_id: int,
-               bucket: str = None,
-               key: str = None) -> None:
-    to_updates = {}
-
-    if bucket is not None:
-        to_updates['bucket'] = bucket
-    if key is not None:
-        to_updates['key'] = key
-
-    if not to_updates:
-        return
-
-    set_sql = ', '.join(fr"{field_name} = %({field_name})s" for field_name in to_updates)
-
-    async with SafeExecutor(
-            event='edit s3_file by id',
-            sql=fr'UPDATE s3_file'
-                fr'   SET {set_sql}'
-                fr' WHERE id = %(s3_file_id)s',
-            s3_file_id=s3_file_id,
-            **to_updates,
-    ):
-        pass
-
-
-async def delete(s3_file_id: int) -> None:
-    async with SafeExecutor(
-            event='delete s3_file',
-            sql=fr'DELETE FROM s3_file'
-                fr'      WHERE id = %(s3_file_id)s',
-            s3_file_id=s3_file_id,
-    ):
-        pass
