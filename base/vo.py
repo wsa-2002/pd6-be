7ba3ff360b26b8cb9d36f07dd08c01e36154e1eb
wsa_2002 <b09705017@ntu.im>
2021-08-04 23:20:34 +0800

    browse class member returns more data

--- base/vo.py
+++ base/vo.py
@@ -6,5 +6,11 @@ from dataclasses import dataclass
 from base import enum
 
 
-
-
+@dataclass
+class BrowseClassMemberOutput:
+    id: int
+    username: str
+    student_id: str
+    real_name: str
+    institute: str
+    role: enum.RoleType
--- persistence/database/class_.py
+++ persistence/database/class_.py
@@ -1,7 +1,7 @@
 from typing import Sequence, Collection, Tuple
 
 import log
-from base import do
+from base import do, vo
 from base.enum import RoleType
 
 from .base import SafeExecutor, SafeConnection
@@ -154,6 +154,27 @@ async def browse_members(class_id: int) -> Sequence[do.Member]:
             fetch='all',
     ) as records:
         return [do.Member(member_id=id_, role=RoleType(role_str)) for id_, role_str in records]
+
+
+async def browse_members_with_accounts(class_id: int) -> Sequence[vo.BrowseClassMemberOutput]:
+    async with SafeExecutor(
+            event='browse class members',
+            sql=fr'SELECT account.id, account.username, account.real_name,'
+                fr'       institute.abbreviated_name,'
+                fr'       student_card.student_id, class_member.role'
+                fr'    FROM account, institute, student_card, class_member'
+                fr'  WHERE class_member.member_id = account.id'
+                fr'    AND class_member.class_id = %(class_id)s'
+                fr'    AND student_card.account_id = account.id'
+                fr'    AND student_card.institute_id = institute.id'
+                fr'    AND student_card.is_default = %(is_default)s',
+            class_id=class_id,
+            is_default=True,
+            fetch='all'
+    ) as records:
+        return [vo.BrowseClassMemberOutput(id=id_, username=username, student_id=student_id, real_name=real_name,
+                                           institute=institute, role=RoleType(role_str))
+                for id_, username, real_name, institute, student_id, role_str in records]
 
 
 async def read_member(class_id: int, member_id: int) -> do.Member:
