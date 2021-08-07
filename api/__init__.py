915ae57ffb91d93a086d1cf3b37aa4a8d990f07d
鄭允頎 <kiyume@ntu.im>
2021-08-07 17:19:03 +0800

    Move register routers back to api

---
+++ api/__init__.py
@@ -0,0 +1,54 @@
+import fastapi
+
+
+def register_routers(app: fastapi.FastAPI):
+    """
+    A function to register routers without possibility of cyclic import.
+    """
+    from .processor import (
+        public,
+
+        institute,
+        account,
+        student_card,
+
+        course,
+        class_,
+        team,
+        grade,
+
+        challenge,
+
+        problem,
+        testcase,
+        submission,
+        judgment,
+
+        peer_review,
+
+        announcement,
+        system,
+    )
+
+    app.include_router(public.router)
+
+    app.include_router(institute.router)
+    app.include_router(account.router)
+    app.include_router(student_card.router)
+
+    app.include_router(course.router)
+    app.include_router(class_.router)
+    app.include_router(team.router)
+    app.include_router(grade.router)
+
+    app.include_router(challenge.router)
+
+    app.include_router(problem.router)
+    app.include_router(testcase.router)
+    app.include_router(submission.router)
+    app.include_router(judgment.router)
+
+    app.include_router(peer_review.router)
+
+    app.include_router(announcement.router)
+    app.include_router(system.router)
