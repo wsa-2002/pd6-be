0d5460407c31f16223c5a92302b1fe244432436a
wsa_2002 <b09705017@ntu.im>
2021-08-10 20:18:09 +0800

    add essay api layer

---
+++ api/service/essay.py
@@ -0,0 +1,8 @@
+import persistence.database as db
+
+
+add = db.essay.add
+read = db.essay.read
+browse = db.essay.browse
+edit = db.essay.edit
+delete = db.essay.delete
---
+++ api/service/essay_submission.py
@@ -0,0 +1,30 @@
+from datetime import datetime
+import typing
+
+import persistence.database as db
+import persistence.s3 as s3
+
+
+read = db.essay_submission.read
+browse = db.essay_submission.browse
+
+
+async def add(file: typing.IO, filename: str, account_id: int, essay_id: int, submit_time: datetime) -> int:
+    bucket, key = await s3.essay_submission.upload(file=file, filename=filename)
+
+    content_file_id = await db.s3_file.add(bucket, key)
+
+    essay_submission_id = await db.essay_submission.add(account_id=account_id, essay_id=essay_id,
+                                                        content_file_id=content_file_id, submit_time=submit_time)
+
+    return essay_submission_id
+
+
+async def edit(file: typing.IO, filename: str, essay_submission_id: int, submit_time: datetime):
+    bucket, key = await s3.essay_submission.upload(file=file, filename=filename)
+
+    content_file_id = await db.s3_file.add(bucket, key)
+
+    await db.essay_submission.edit(essay_submission_id=essay_submission_id,
+                                   content_file_id=content_file_id,
+                                   submit_time=submit_time)
