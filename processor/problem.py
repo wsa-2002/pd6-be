8dc9009772c80dbddce36f3d87949c31fb1d050e
wsa_2002 <b09705017@ntu.im>
2021-08-17 15:07:13 +0800

    Enable/disable testcase

--- processor/problem.py
+++ processor/problem.py
@@ -63,6 +63,7 @@ async def read_problem(problem_id: int, request: Request) -> do.Problem:
 class EditProblemInput(BaseModel):
     title: str = None
     full_score: int = None
+    testcase_disabled: bool = None
     description: Optional[str] = ...
     source: Optional[str] = ...
     hint: Optional[str] = ...
@@ -82,8 +83,8 @@ async def edit_problem(problem_id: int, data: EditProblemInput, request: Request
         raise exc.NoPermission
 
     return await service.problem.edit(problem_id, title=data.title, full_score=data.full_score,
-                                      description=data.description, source=data.source,
-                                      hint=data.hint)
+                                      testcase_disabled=data.testcase_disabled, description=data.description,
+                                      source=data.source, hint=data.hint)
 
 
 @router.delete('/problem/{problem_id}')
--- service/problem.py
+++ service/problem.py
@@ -1,10 +1,26 @@
+from typing import Optional
+
 import persistence.database as db
 
 
 add = db.problem.add
 browse = db.problem.browse
 read = db.problem.read
-edit = db.problem.edit
 delete = db.problem.delete_cascade
 
 browse_problem_set = db.problem.browse_problem_set
+
+
+async def edit(problem_id: int,
+               title: str = None,
+               full_score: int = None,
+               testcase_disabled: bool = None,
+               description: Optional[str] = ...,
+               source: Optional[str] = ...,
+               hint: Optional[str] = ...,) -> None:
+
+    await db.problem.edit(problem_id, title=title, full_score=full_score,
+                          description=description, source=source, hint=hint)
+
+    await db.testcase.disable_enable_testcase_by_problem(problem_id=problem_id,
+                                                         testcase_disabled=testcase_disabled)
