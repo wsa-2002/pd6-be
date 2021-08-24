4bc3b914b3d59756cc260c7b1e56c3601e227e36
wsa_2002 <b09705017@ntu.im>
2021-08-24 23:05:10 +0800

    fix auth issue and bugs

--- persistence/database/judgment.py
+++ persistence/database/judgment.py
@@ -1,5 +1,4 @@
 from typing import Sequence
-from datetime import datetime
 
 from base import do, enum
 
@@ -26,7 +25,7 @@ async def read(judgment_id: int) -> do.Judgment:
             event='read judgment',
             sql=fr'SELECT submission_id, status, total_time, max_memory, score, judge_time'
                 fr'  FROM judgment'
-                fr' WHERE judgment_id = %(judgment_id)s',
+                fr' WHERE id = %(judgment_id)s',
             judgment_id=judgment_id,
             fetch=1,
     ) as (submission_id, status, total_time, max_memory, score, judge_time):
@@ -64,43 +63,3 @@ async def read_case(judgment_id: int, testcase_id: int) -> do.JudgeCase:
     ) as (judgment_id, testcase_id, status, time_lapse, peak_memory, score):
         return do.JudgeCase(judgment_id=judgment_id, testcase_id=testcase_id, status=enum.JudgmentStatusType(status),
                             time_lapse=time_lapse, peak_memory=peak_memory, score=score)
-
-
-async def get_submission_score(problem_id: int, account_id: int, selection_type: enum.TaskSelectionType,
-                               challenge_end_time: datetime) -> do.Judgment:
-    if selection_type is enum.TaskSelectionType.last:
-        async with SafeExecutor(
-                event='get submission score by LAST',
-                sql=fr'SELECT judgment.id, judgment.submission_id, judgment.status, judgment.total_time,'
-                    fr'       judgment.max_memory, judgment.score, judgment.judge_time'
-                    fr'  FROM judgment'
-                    fr' INNER JOIN submission'
-                    fr'         ON submission.id = judgment.submission_id'
-                    fr'        AND submission.account_id = %(account_id)s'
-                    fr'        AND submission.submit_time <= %(challenge_end_time)s'
-                    fr'        AND submission.problem_id = %(problem_id)s'
-                    fr'   ORDER BY submission.id DESC'
-                    fr' LIMIT 1',
-                account_id=account_id, challenge_end_time=challenge_end_time, problem_id=problem_id,
-                fetch=1,
-        ) as (id_, submission_id, status, total_time, max_memory, score, judge_time):
-            return do.Judgment(id=id_, submission_id=submission_id, status=status, total_time=total_time,
-                               max_memory=max_memory, score=score, judge_time=judge_time)
-
-    elif selection_type is enum.TaskSelectionType.best:
-        async with SafeExecutor(
-                event='get submission score by BEST',
-                sql=fr'SELECT judgment.id, judgment.submission_id, judgment.status, judgment.total_time,'
-                    fr'       judgment.max_memory, judgment.score, judgment.judge_time'
-                    fr'  FROM judgment'
-                    fr' INNER JOIN submission'
-                    fr'         ON submission.id = judgment.submission_id'
-                    fr'        AND submission.account_id = %(account_id)s'
-                    fr'        AND submission.submit_time <= %(challenge_end_time)s'
-                    fr'        AND submission.problem_id = %(problem_id)s'
-                    fr' ORDER BY judgment.score DESC'
-                    fr' LIMIT 1',
-                account_id=account_id, challenge_end_time=challenge_end_time, problem_id=problem_id,
-        ) as (id_, submission_id, status, total_time, max_memory, score, judge_time):
-            return do.Judgment(id=id_, submission_id=submission_id, status=status, total_time=total_time,
-                               max_memory=max_memory, score=score, judge_time=judge_time)
