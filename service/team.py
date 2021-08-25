b49225f3a2b921c404a6c9b4f9359e5a121a4582
wsa_2002 <b09705017@ntu.im>
2021-08-25 14:03:07 +0800

    use on conflict while import team

--- service/team.py
+++ service/team.py
@@ -46,9 +46,5 @@ async def replace_members(team_id: int, member_roles: Sequence[Tuple[str, enum.R
 async def import_team(team_file: typing.IO, class_id: int):
     rows = csv.DictReader(codecs.iterdecode(team_file, 'utf_8_sig'))
     for row in rows:
-        try:
-            team = await db.team.read_by_team_name(class_id=class_id, team_name=row['TeamName'], label=row['Label'])
-            team_id = team.id
-        except exc.persistence.NotFound:
-            team_id = await db.team.add(name=row['TeamName'], class_id=class_id, label=row['Label'])
-        await db.team.add_member(team_id=team_id, account_referral=row['TeamMember'], role=enum.RoleType(row['Role']))
+        await db.team.add_team_and_add_member(team_name=row['TeamName'], class_id=class_id, team_label=row['Label'],
+                                              account_referral=row['TeamMember'], role=enum.RoleType(row['Role']))
