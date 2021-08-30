6667002b9ad8b411e4f702eb7cc9bdce04cfb993
wsa_2002 <b09705017@ntu.im>
2021-08-30 22:45:22 +0800

    Modify  grade template columns

--- service/grade.py
+++ service/grade.py
@@ -11,7 +11,7 @@ import persistence.s3 as s3
 from base import do
 
 
-GRADE_TEMPLATE = b'Receiver,Title,Score,Comment,Grader'
+GRADE_TEMPLATE = b'Receiver,Score,Comment,Grader\nB05705088,10,here for comment,B99705006'
 GRADE_TEMPLATE_FILENAME = 'grade_template.csv'
 
 
@@ -22,11 +22,11 @@ read = db.grade.read
 delete = db.grade.delete
 
 
-async def import_class_grade(grade_file: typing.IO, class_id: int, update_time: datetime):
+async def import_class_grade(grade_file: typing.IO, title: str, class_id: int, update_time: datetime):
     rows = csv.DictReader(codecs.iterdecode(grade_file, 'utf_8_sig'))
     for row in rows:
         await db.grade.add(receiver=row['Receiver'], grader=row['Grader'], class_id=class_id,
-                           comment=row['Comment'], score=row['Score'], title=row['Title'],
+                           comment=row['Comment'], score=row['Score'], title=title,
                            update_time=update_time)
 
 
