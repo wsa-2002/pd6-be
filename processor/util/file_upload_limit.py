73e65271f7e86484c5c9b3c9312588f1087f0e1e
wsa_2002 <b09705017@ntu.im>
2021-08-31 23:22:17 +0800

    fix bug

--- processor/util/file_upload_limit.py
+++ processor/util/file_upload_limit.py
@@ -2,6 +2,6 @@ from fastapi import Header
 
 
 def valid_file_length(file_length: int):
-    async def validator(content_length: int = Header(..., lt=file_length)):  # 1mb
+    async def validator(content_length: int = Header(..., lt=file_length)):
         return content_length
     return validator
