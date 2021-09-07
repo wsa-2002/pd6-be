2bb642c35e48d29aee30e418bc785de065a8369d
wsa_2002 <b09705017@ntu.im>
2021-09-07 21:44:38 +0800

    Sort browse email verification

--- persistence/database/email_verification.py
+++ persistence/database/email_verification.py
@@ -11,7 +11,8 @@ async def browse(account_id: int, is_consumed=False) -> Sequence[do.EmailVerific
             sql=fr'SELECT id, account_id, institute_id, student_id, email, is_consumed'
                 fr'  FROM email_verification'
                 fr' WHERE account_id = %(account_id)s'
-                fr'   AND is_consumed = %(is_consumed)s',
+                fr'   AND is_consumed = %(is_consumed)s'
+                fr' ORDER BY id ASC',
             account_id=account_id, is_consumed=is_consumed,
             fetch='all',
             raise_not_found=False,
