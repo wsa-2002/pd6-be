a7f4fb0538e3639c51ac359b51b84af7b3436e80
鄭允頎 <kiyume@ntu.im>
2021-09-27 00:57:26 +0800

    fix count

--- persistence/database/view.py
+++ persistence/database/view.py
@@ -50,8 +50,12 @@ async def account(limit: int, offset: int, filters: list[Filter], sorters: list[
 
     total_count = await execute_count(
         sql=fr'SELECT *'
-            fr'  FROM view_account'
-            fr'{f" WHERE {cond_sql}" if cond_sql else ""}',
+            fr'  FROM account'
+            fr'  LEFT JOIN student_card'
+            fr'         ON student_card.account_id = account.id'
+            fr'        AND student_card.is_default'
+            fr' WHERE NOT account.is_deleted'
+            fr'{f" AND {cond_sql}" if cond_sql else ""}',
         **cond_params,
     )
 
@@ -116,7 +120,15 @@ async def class_member(limit: int, offset: int, filters: Sequence[Filter], sorte
 
     total_count = await execute_count(
         sql=fr'SELECT *'
-            fr'  FROM view_class_member'
+            fr'  FROM class_member'
+            fr' INNER JOIN account'
+            fr'         ON class_member.member_id = account.id'
+            fr'        AND NOT account.is_deleted'
+            fr'  LEFT JOIN student_card'
+            fr'         ON account.id = student_card.account_id'
+            fr'        AND student_card.is_default'
+            fr'  LEFT JOIN institute'
+            fr'         ON student_card.institute_id = institute.id'
             fr'{f" WHERE {cond_sql}" if cond_sql else ""}',
         **cond_params,
     )
@@ -210,8 +222,38 @@ async def class_submission(class_id: int, limit: int, offset: int,
 
     total_count = await execute_count(
         sql=fr'SELECT *'
-            fr'  FROM view_submission_under_class'
-            fr'{f" WHERE {cond_sql}" if cond_sql else ""}',
+            fr'FROM ('
+            fr'    SELECT DISTINCT ON (submission.id)'
+            fr'           submission.id           AS submission_id,'
+            fr'           account.id              AS account_id,'
+            fr'           account.username        AS username,'
+            fr'           student_card.student_id AS student_id,'
+            fr'           account.real_name       AS real_name,'
+            fr'           problem.challenge_id    AS challenge_id,'
+            fr'           challenge.title         AS challenge_title,'
+            fr'           problem.id              AS problem_id,'
+            fr'           problem.challenge_label AS challenge_label,'
+            fr'           judgment.verdict        AS verdict,'
+            fr'           submission.submit_time  AS submit_time,'
+            fr'           challenge.class_id      AS class_id'
+            fr'      FROM submission'
+            fr'      LEFT JOIN account'
+            fr'             ON account.id = submission.account_id'
+            fr'            AND NOT account.is_deleted'
+            fr'      LEFT JOIN student_card'
+            fr'             ON student_card.account_id = submission.account_id'
+            fr'            AND student_card.is_default'
+            fr'      LEFT JOIN problem'
+            fr'             ON problem.id = submission.problem_id'
+            fr'            AND NOT problem.is_deleted'
+            fr'      LEFT JOIN challenge'
+            fr'             ON challenge.id = problem.challenge_id'
+            fr'            AND NOT challenge.is_deleted'
+            fr'      LEFT JOIN judgment'
+            fr'             ON judgment.submission_id = submission.id'
+            fr'    {f" WHERE {cond_sql}" if cond_sql else ""}'
+            fr'     ORDER BY submission.id DESC, judgment.judge_time DESC'
+            fr') __TABLE__',
         **cond_params,
         class_id=class_id,
     )
@@ -302,8 +344,40 @@ async def my_submission(limit: int, offset: int, filters: Sequence[Filter], sort
 
     total_count = await execute_count(
         sql=fr'SELECT *'
-            fr'  FROM view_my_submission'
-            fr'{f" WHERE {cond_sql}" if cond_sql else ""}',
+            fr'FROM ('
+            fr'    SELECT DISTINCT ON (submission.id)'
+            fr'           submission.id           AS submission_id,'
+            fr'           course.id               AS course_id,'
+            fr'           course.name             AS course_name,'
+            fr'           class.id                AS class_id,'
+            fr'           class.name              AS class_name,'
+            fr'           challenge.id            AS challenge_id,'
+            fr'           challenge.title         AS challenge_title,'
+            fr'           problem.id              AS problem_id,'
+            fr'           problem.challenge_label AS challenge_label,'
+            fr'           judgment.verdict        AS verdict,'
+            fr'           submission.submit_time  AS submit_time,'
+            fr'           account.id              AS account_id'
+            fr'      FROM submission'
+            fr'      LEFT JOIN judgment'
+            fr'             ON submission.id = judgment.submission_id'
+            fr'      LEFT JOIN problem'
+            fr'             ON problem.id = submission.problem_id'
+            fr'            AND NOT problem.is_deleted'
+            fr'      LEFT JOIN challenge'
+            fr'             ON challenge.id = problem.challenge_id'
+            fr'            AND NOT challenge.is_deleted'
+            fr'      LEFT JOIN class'
+            fr'             ON class.id = challenge.class_id'
+            fr'            AND NOT class.is_deleted'
+            fr'      LEFT JOIN course'
+            fr'             ON course.id = class.course_id'
+            fr'            AND NOT course.is_deleted'
+            fr'      LEFT JOIN account'
+            fr'             ON account.id = submission.account_id'
+            fr' {f" WHERE {cond_sql}" if cond_sql else ""}'
+            fr'     ORDER BY submission.id DESC, judgment.id DESC'
+            fr') __TABLE__',
         **cond_params,
     )
 
