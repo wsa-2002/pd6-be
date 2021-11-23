529fdecac13d56fd942c9f58dd908ecd305069ad
= <kogby0507@gmail.com>
2021-11-23 19:49:26 +0800

    fix scb and need to refine SQL

--- persistence/database/judge_case.py
+++ persistence/database/judge_case.py
@@ -1,11 +1,10 @@
 from typing import Iterable
 
-from base import do
-
+from base import do, enum
 from .base import FetchAll
 
 
-async def batch_get_with_judgment(testcase_id: int, judgment_ids: Iterable[int]) -> dict[int, do.JudgeCase]:
+async def batch_get_with_judgment(testcase_id: int, judgment_ids: Iterable[int], verdict: enum.VerdictType = None) -> dict[int, do.JudgeCase]:
     cond_sql = ', '.join(str(judgment_id) for judgment_id in judgment_ids)
     async with FetchAll(
             event='batch get judge case with judgment ids',
@@ -15,7 +14,8 @@ async def batch_get_with_judgment(testcase_id: int, judgment_ids: Iterable[int])
                 fr' INNER JOIN testcase'
                 fr'         ON testcase.id = judge_case.testcase_id'
                 fr'      WHERE judge_case.testcase_id = %(testcase_id)s'
-                fr'{f"     AND judge_case.judgment_id IN ({cond_sql})" if cond_sql else ""}',
+                fr'{f"     AND judge_case.judgment_id IN ({cond_sql})" if cond_sql else ""}'
+                fr"        AND judge_case.verdict = '{verdict}'" if verdict  else "",
             testcase_id=testcase_id,
             raise_not_found=False,  # Issue #134: return [] for browse
     ) as records:
@@ -23,3 +23,6 @@ async def batch_get_with_judgment(testcase_id: int, judgment_ids: Iterable[int])
                                           time_lapse=time_lapse, peak_memory=peak_memory, score=score)
                 for (judgment_id, testcase_id, verdict, time_lapse, peak_memory, score)
                 in records}
+
+
+
