154e22640c129f9d7983a719e2cb1c1c944420c7
wsa-2002 <bensonwang2001@gmail.com>
2022-03-02 00:49:54 +0800

    modify event name

--- persistence/database/s3_file.py
+++ persistence/database/s3_file.py
@@ -24,7 +24,7 @@ async def browse_with_uuids(uuids: Iterable[UUID]) -> Sequence[Optional[do.S3Fil
     if not value_sql:
         return []
 
-    async with SafeConnection(event='browse account referral with ids',
+    async with SafeConnection(event='browse s3 file with uuids',
                               auto_transaction=True) as conn:
         return [do.S3File(uuid=uuid, bucket=bucket, key=key)
                 if uuid else None
