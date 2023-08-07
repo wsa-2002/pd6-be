d8e3df8813ad0c232b991e6d5e4a8d3fa7293f74
鄭允頎 <kiyume@ntu.im>
2023-08-07 23:21:08 +0800

    add AnySetOfValues

--- util/mock/compare.py
+++ util/mock/compare.py
@@ -18,3 +18,22 @@ class AnyInstanceOf:
 
     def __repr__(self):
         return f'<ANY {self._target_class} instance>'
+
+
+class AnySetOfValues:
+    """
+    A helper object that compares equal to any instance of a specific class.
+    Implemented with reference of unittest.mock.ANY.
+    """
+
+    def __init__(self, values: typing.Iterable):
+        self._set_of_values = set(values)
+
+    def __eq__(self, other):
+        return self._set_of_values.__eq__(set(other))
+
+    def __ne__(self, other):
+        return self._set_of_values.__ne__(set(other))
+
+    def __repr__(self):
+        return f'<ANY {self._set_of_values} value set>'
