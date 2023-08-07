e4e6d3a89a4477dabd2cd42bebb0878e81b7336f
鄭允頎 <kiyume@ntu.im>
2023-08-07 23:09:20 +0800

    add global mock func

--- util/mock/mock.py
+++ util/mock/mock.py
@@ -161,6 +161,7 @@ class Controller:
     def __init__(self):
         self._calls: list[CallRecord] = list()
         self._unpatch_funcs: list[typing.Callable] = list()
+        self._global_module = MockModule(self, '')
 
     def __enter__(self):
         return self
@@ -172,14 +173,25 @@ class Controller:
         if self._calls:
             raise AssertionError(f'{len(self._calls)} expected call(s) not fulfilled, next one is {self._calls[0]}')
 
-    def mock_module(self, module_name: str) -> MockModule:
-        module = MockModule(self, module_name)
-
-        patched = patch(module_name, module)
+    def _patch(self, name, mocked):
+        patched = patch(name, mocked)
         patched.__enter__()
         self._unpatch_funcs.append(patched.__exit__)
 
+    def mock_module(self, module_name: str) -> MockModule:
+        module = MockModule(self, module_name)
+        self._patch(module_name, module)
         return module
+
+    def mock_global_func(self, func_name: str) -> MockFunction:
+        mocked = self._global_module.func(func_name)
+        self._patch(func_name, mocked)
+        return mocked
+
+    def mock_global_async_func(self, func_name: str) -> MockAsyncFunction:
+        mocked = self._global_module.async_func(func_name)
+        self._patch(func_name, mocked)
+        return mocked
 
     def _register_call(self, record: CallRecord):
         self._calls.append(record)
